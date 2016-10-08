ee1e598f12ef112d47993c57be4db7289a070abd
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-10-08 20:47:18 +0000

    Created function to delete all launch configurations and some cleanup work

--- destroy-env.sh
+++ destroy-env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Name: Tarun Gidwani
+# Script name: destroy-env.sh
+# Purpose: Destroys all EC2 objects in default region
 
-
-
-
+log_file=delete-log.txt
 
 # Deletes all auto scaling groups and terminates
 # all instances attached to it in the default
@@ -15,22 +16,35 @@ function delete_all_auto_scaling_groups
 	local desired_capacity=0
 	local query_auto_scaling_group_cmd=""
 	local auto_scaling_group_names=($(aws autoscaling describe-auto-scaling-groups --output "text"\
-                                     --query "AutoScalingGroups[*].AutoScalingGroupName" 2>> delete-log.txt))
+                                     --query "AutoScalingGroups[*].AutoScalingGroupName" 2>> $log_file))
 	for auto_scaling_group_name in "${auto_scaling_group_names[@]}"
 	do
 		local instances_of_asg=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group $auto_scaling_group_name\
-														 --query "AutoScalingGroups[].Instances[].InstanceId" --output "text" 2>> delete-log.txt)
+														 --query "AutoScalingGroups[].Instances[].InstanceId" --output "text" 2>> $log_file)
 
 		aws autoscaling update-auto-scaling-group --auto-scaling-group-name $auto_scaling_group_name\
-		--min-size $min_size --max-size $max_size --desired-capacity $desired_capacity 2>> delete-log.txt
-		aws ec2 wait instance-terminated --instance-ids $instances_of_asg 2>> delete-log.txt
-		aws autoscaling describe-scaling-activities --auto-scaling-group-name $auto_scaling_group_name &>> sa.txt
+		--min-size $min_size --max-size $max_size --desired-capacity $desired_capacity 2>> $log_file
+		aws ec2 wait instance-terminated --instance-ids $instances_of_asg 2>> $log_file
 		# Even after waiting for the instances attached to the auto-scaling-group to terminate, we run
 		# run into the issue of ScalingActivityInProgress (some cleanup still in progress) and this 
 		# prevents the auto-scaling-groups from being deleted. So we have to either sleep for a 
 		# arbitrary amount of time or use the --force-delete option
 		aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $auto_scaling_group_name \
-		--force-delete 2>> delete-log.txt
+		--force-delete 2>> $log_file
+	done
+}
+
+# Deletes all launch configurations
+# in the default region
+function delete_all_launch_configurations 
+{
+	local all_launch_configurations=($(aws autoscaling describe-launch-configurations \
+																		 --query "LaunchConfigurations[*].LaunchConfigurationName" \
+																		 --output text 2>> $log_file))
+	for launch_configuration in ${all_launch_configurations[@]}
+	do
+		aws autoscaling delete-launch-configuration --launch-configuration-name $launch_configuration \
+		2>> $log_file
 	done
 }
 
@@ -38,8 +52,12 @@ function delete_all_auto_scaling_groups
 # Auto-scaling-groups
 function destroy_env
 {
-	printf "Deleting auto-scaling-groups.........\n"
+	printf "Deleting all auto-scaling-groups.........\n"
 	delete_all_auto_scaling_groups
+	printf "Completed successfully!\n"
+
+	printf "Deleting all launch configurations.........\n"
+	delete_all_launch_configurations
 	printf "Completed successfully!\n"
 }
 
