c10e6258106452c0fe47cde7116f88a6b907fe6b
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-04 17:03:53 -0500

    Added function to generate create table query

--- web-app/generate_sql_queries.php
+++ web-app/generate_sql_queries.php
@@ -4,3 +4,15 @@
     {
         return "CREATE DATABASE $db_name";
     }
+
+    function create_students_table_query($db_name, $table_name)
+    {
+        return <<<SQL
+  CREATE TABLE $db_name.$table_name
+  (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255),
+    Age INT(3)
+  );
+SQL;
+    }
