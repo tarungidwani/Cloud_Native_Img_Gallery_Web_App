fe72afff79de4558f1d6594c56fb9b25b56ed2a3
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-04 17:21:17 -0500

    Added function to dynmically retrieve the endpoint of DB instance running on AWS's RDS

--- web-app/db_creation.php
+++ web-app/db_creation.php
@@ -1,4 +1,7 @@
 <?php
+
+    require 'aws_sdk/aws-autoloader.php';
+
     define('PATH_TO_DB_CONNECTION_INFO_FILE' , 'config/db_connection');
 
     /* Reads the db connection config file, parses it,
@@ -17,3 +20,36 @@
             exit(1);
         }
     }
+
+    /* Querys AWS's RDS to get the endpoint of the DB instance
+     * whose db-identifer and region are passed to this
+     * function as arguments
+     * Note: Credentials will be removed, currently added
+     *       for testing purposes
+    */
+    function get_db_endpoint($region, $db_identifier)
+    {
+        $rds_client = new \Aws\Rds\RdsClient([
+            'version' => 'latest',
+            'region'  => "$region",
+            'credentials' => [
+                'key' => 'AKIAINSA32Q2CDCUR4OA',
+                'secret' => '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'
+            ]
+        ]);
+
+        try
+        {
+            $db_info = $rds_client->describeDBInstances([
+                'DBInstanceIdentifier' => "$db_identifier"
+            ]);
+            return $db_info['DBInstances'][0]['Endpoint']['Address'];
+        }
+        catch (\Aws\Rds\Exception\RdsException $rds_exception)
+        {
+            echo "RDS instance $db_identifier does not exist, please create required instance by running the create-app-env.sh script\n";
+            exit(1);
+        }
+    }
+
+
