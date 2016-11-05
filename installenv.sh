b07ce32f2940ff1bd64cb705932858beed49f308
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-05 14:02:47 -0500

    Added commands to install apache2 web server and start it up

--- installenv.sh
+++ installenv.sh
@@ -9,4 +9,11 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
+# Installs and starts
+# the apache2 web server
+sudo apt-get -y install apache2
+sudo service apache2 start
+
+
+
 
