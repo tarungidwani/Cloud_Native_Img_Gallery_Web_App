f2666216088155c51e609a003f58826e627bbe65
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-05 14:16:13 -0500

    Added commands to install all tools needed to install php and its modules

--- installenv.sh
+++ installenv.sh
@@ -14,6 +14,9 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y install apache2
 sudo service apache2 start
 
-
-
+# Installs all tools needed
+# to install and setup php
+sudo apt-get -y install zip
+sudo apt-get -y install unzip
+sudo apt-get -y install curl
 
