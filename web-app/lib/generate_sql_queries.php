6bcab893260393c526b4a0e7325145d21a97284f
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-04 22:19:56 -0500

    Updated select all student records statement to sort ascending by ID

--- web-app/lib/generate_sql_queries.php
+++ web-app/lib/generate_sql_queries.php
@@ -34,5 +34,5 @@ SQL;
 
     function create_select_all_records_query($db_name, $table_name)
     {
-        return "SELECT * FROM $db_name.$table_name";
+        return "SELECT * FROM $db_name.$table_name ORDER BY ID";
     }
