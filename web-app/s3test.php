1ec68383f3a1a411a8bf83680ef80a7ba9389d6f
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-05 20:55:06 -0500

    Added comment for display image function

--- web-app/s3test.php
+++ web-app/s3test.php
@@ -5,8 +5,14 @@
 
     define('PATH_TO_S3_CONNECTION_INFO_FILE' , 'config/s3_connection');
 
+    /* Displays an image on the
+     * screen using a S3 bucket
+     * as the src
+    */
     function display_image($file_url)
     {
         echo "<h2>URL to img in AWS Raw S3 bucket: $file_url</h2>";
         echo "<img src='$file_url' alt='T-REX with a switch and Microsoft/IIT flag with grabbers in its hands' width='90%' height='90%'>";
     }
+
+
