927592d4c2ab08b13a2aac3746efd4e84fe37e05
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-06 22:11:47 -0600

    Created function to create security group for rds (mariadb) instance

--- setup-web-app.sh
+++ setup-web-app.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 ec2_security_group="gallery-web-app-sec-group"
+rds_security_group="gallery-web-app-db-sec-group"
 
 # Creates a new security group 
-# Adds rule to allow inbound traffic from any host on port 22 (ssh)
-# Adds rule to allow inbound traffic from any host on port 80 (http)
+# Adds rule to allow inbound traffic 
+# from any host on port 22 (ssh)
+# Adds rule to allow inbound traffic 
+# from any host on port 80 (http)
 function create_ec2_sec_group
 {
 	
@@ -24,10 +27,30 @@ function create_ec2_sec_group
 	printf "Completed Successfully\n"
 }
 
+# Creates a new security group
+# Adds rule to allow inbound traffic 
+# from any host on port 3306 (ssh)
+function create_rds_sec_group
+{
+	local rds_security_group_description="Allows inbound traffic on 3306 default port for MySQL/MariaDB"
+
+	printf "\nCreating security group for RDS (MySQL/MariaDB) instance\n"
+
+	aws ec2 create-security-group --group-name "$rds_security_group" --description "$rds_security_group_description" \
+	&> /dev/null
+
+	aws ec2 authorize-security-group-ingress --group-name "$rds_security_group" --protocol tcp --port 3306 \
+ 	--cidr 0.0.0.0/0 &> /dev/null
+
+	printf "Completed Successfully\n"
+}
+
 # Destroy all existing infrastructure
 # before begining setup of infra and
 # web app
 ./destroy-env.sh
 
 create_ec2_sec_group
+
+create_rds_sec_group
 
