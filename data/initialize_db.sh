5c02011f68b65f0d1e3d659ccb9452134bb87038
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-10 16:33:56 -0600

    Added commands to create user's login information and added comments as well

--- data/initialize_db.sh
+++ data/initialize_db.sh
@@ -13,9 +13,17 @@ then
 	exit 1;
 fi
 
-mysql -h "$host" -u "$db_username" -p"$db_password" << EOF 
-
+mysql -h "$host" -u "$db_username" -p"$db_password" 2>> db_initialize.txt << EOF 
+	
+	/* Creates a database */
 	CREATE DATABASE IF NOT EXISTS $db_name;
 	USE $db_name;
+
+	/* Creates a table */
+	CREATE TABLE IF NOT EXISTS $table_name
+	(
+		user_name VARCHAR(255) PRIMARY KEY,
+		password VARCHAR(255) NOT NULL
+	);
 
 EOF
