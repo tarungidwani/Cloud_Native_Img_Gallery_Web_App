5230a4327c7c5c3cc03d530e4d2118f271250d8f
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-11 00:15:05 -0600

    Added create table command to create a table to store all the image processing jobs and their information

--- data/initialize_db.sh
+++ data/initialize_db.sh
@@ -12,6 +12,8 @@ password_1=$(md5sum <<< 'LetMeInTarun')
 password_2=$(md5sum <<< 'LetMeInJeremy')
 password_3=$(md5sum <<< 'LetMeInController')
 
+table_name_1="jobs"
+
 if [ $# != 1 ]
 then
 	printf "\n**Please provide url to DB instance endpoint**\n\n"
@@ -19,7 +21,7 @@ then
 fi
 
 mysql -h "$host" -u "$db_username" -p"$db_password" 2>> db_initialize.txt << EOF 
-	
+
 	/* Creates a database */
 	CREATE DATABASE IF NOT EXISTS $db_name;
 	USE $db_name;
@@ -38,6 +40,18 @@ mysql -h "$host" -u "$db_username" -p"$db_password" 2>> db_initialize.txt << EOF
 	VALUES 
 	('tgidwani@hawk.iit.edu', "$password_1"),
 	('hajek@iit.edu'        , "$password_2"),
-	('controller'           , "$password_3")
+	('controller'           , "$password_3");
+	
+	/* Creates a table */
+	CREATE TABLE IF NOT EXISTS $table_name_1
+	(
+		id INT PRIMARY KEY AUTO_INCREMENT,
+		user_login_id INT NOT NULL,
+		phone_number VARCHAR(20),
+		s3_raw_url VARCHAR(2083) NOT NULL,
+		s3_finished_url VARCHAR(2083),
+		status ENUM('0','1') NOT NULL,
+		reciept VARCHAR(32) NOT NULL
+	);
 
 EOF
