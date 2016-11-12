bcb4b5fe42464c9c6f5c36d15fcb171e5d5e2698
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-12 16:43:24 -0600

    Created the welcome page for this app after successful authenticaiton with different view for user vs. admin

--- web-app/pages/welcome.php
+++ web-app/pages/welcome.php
@@ -1,1 +1,28 @@
 <?php
+
+    session_start();
+    $features = ["<li><a href=\"gallery.php\">Gallery</a></li>",
+                 "<li><a href=\"upload.php\">Upload</a></li>"];
+
+    if($_SESSION['client_token'] == null)
+        header('location: ../index.php');
+
+    if($_SESSION['is_admin'])
+        $features[] = "<li><a href=\"admin.php\">Admin</a></li>";
+?>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+<ul>
+    <?php
+    foreach($features as $feature)
+        print "$feature";
+    ?>
+</ul>
+</body>
+</html>
