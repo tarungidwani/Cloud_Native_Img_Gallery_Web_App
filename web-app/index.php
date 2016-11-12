ee91b403abf193366ea3e5ebbe09b4f0ca6b92d4
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-12 11:38:35 -0600

    Made username and password input fields required

--- web-app/index.php
+++ web-app/index.php
@@ -7,15 +7,12 @@
 <body>
 <div id = "center box" style = "height: 200px; width: 400px; position: fixed; top: 50%; left: 50%; margin-top: -100px; margin-left: -200px;">
     <form method="post">
-        Username <input type="text" name="username"/>
+        Username <input type="text" name="user_name" required/>
         <br><br>
-        Password <input type="password" name="password"/>
+        Password <input type="password" name="password" required/>
         <br><br>
         <input type="submit" name="submit" value="Login"/>
     </form>
 </div>
 </body>
 </html>
-
-
-
