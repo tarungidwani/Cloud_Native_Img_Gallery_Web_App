7e59bee861c9154deea5c4e5395046a0d55631a4
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-11 20:18:08 -0600

    Creates a new index.php page with a login form

---
+++ web-app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Login In</title>
+</head>
+<body>
+<div id = "center box" style = "height: 200px; width: 400px; position: fixed; top: 50%; left: 50%; margin-top: -100px; margin-left: -200px;">
+    <form method="post">
+        Username <input type="text" name="username"/>
+        <br><br>
+        Password <input type="password" name="password"/>
+        <br><br>
+        <input type="submit" name="submit" value="Login"/>
+    </form>
+</div>
+</body>
+</html>
+
+
+
