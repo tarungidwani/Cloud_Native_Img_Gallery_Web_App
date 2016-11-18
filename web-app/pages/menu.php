2cbeb08e9a8d03576cd2ff39d5cac7af1b51bd78
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-18 01:02:00 -0600

    Created a menu page that will be display all the enabled features of the gallery web app at the top of each page

---
+++ web-app/pages/menu.php
@@ -0,0 +1,33 @@
+<?php
+
+    require '../lib/feature_status.php';
+
+    function get_enabled_features()
+    {
+        $features = ["<li><a href=\"gallery.php\">Gallery</a></li>"];
+        $is_upload_feature_enabled = is_feature_enabled('upload');
+
+        if($is_upload_feature_enabled)
+            $features[] = "<li><a href=\"upload.php\">Upload</a></li>";
+
+        $a = $_SESSION['is_admin'];
+        if($_SESSION['is_admin'])
+            $features[] = "<li><a href=\"admin.php\">Admin</a></li>";
+        return $features;
+    }
+    $features = get_enabled_features();
+?>
+
+<ul>
+    <?php
+    foreach($features as $feature)
+        print "$feature";
+    ?>
+</ul>
+
+
+
+
+
+
+
