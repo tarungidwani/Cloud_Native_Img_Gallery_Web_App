52b4953b086d6a65d941e54ca4fed2fd962dccf8
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-21 12:05:39 -0600

    Created form to upload and submit an image

--- web-app/pages/upload.php
+++ web-app/pages/upload.php
@@ -3,3 +3,17 @@
     require 'menu.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Upload</title>
+</head>
+<body>
+<form method="post" enctype="multipart/form-data">
+    <input type="file" accept="image/*"  name="img_path" style="padding-bottom: 20px;padding-top: 20px;" />
+    <br>
+    <input type="submit" value="Upload" formaction="uploader.php"/>
+</form>
+</body>
+</html>
