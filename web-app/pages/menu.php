a18cf07644556ce15a2414556b30aa7d9309c3e9
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-21 11:37:23 -0600

    Imported init.php in menu.php

--- web-app/pages/menu.php
+++ web-app/pages/menu.php
@@ -1,5 +1,6 @@
 <?php
 
+    require 'init.php';
     require '../lib/feature_status.php';
 
     function get_enabled_features()
@@ -10,7 +11,6 @@
         if($is_upload_feature_enabled)
             $features[] = "<li><a href=\"upload.php\">Upload</a></li>";
 
-        $a = $_SESSION['is_admin'];
         if($_SESSION['is_admin'])
             $features[] = "<li><a href=\"admin.php\">Admin</a></li>";
         return $features;
