02324cb3fe0972dd05e61c34a5ef472690b51938
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-23 16:41:03 -0600

    Added commands to install and setup php

--- install-worker-env.sh
+++ install-worker-env.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# Installs all tools needed
+# to install and setup php
+run-one-until-success sudo apt-get -y install zip
+run-one-until-success sudo apt-get -y install unzip
+run-one-until-success sudo apt-get -y install curl
+
+# Installs php and all the
+# required modules
+run-one-until-success sudo apt-get -y install php
+run-one-until-success sudo apt-get -y install php-xml
+run-one-until-success sudo apt-get -y install php-curl
+run-one-until-success sudo apt-get -y install php-mysql
+
