d5603a1e5733ec78ae11bec218da554ce93aace1
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-24 11:35:12 -0600

    Added commands to download and setup AWS PHP SDK

--- install-worker-env.sh
+++ install-worker-env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+web_app_dir=/home/ubuntu/web-app
+aws_sdk_tmp_location=/tmp/aws.zip
+aws_sdk_location=$web_app_dir/aws_sdk
+aws_sdk_url=http://docs.aws.amazon.com/aws-sdk-php/v3/download/aws.zip
+
 # Installs all tools needed
 # to install and setup php
 run-one-until-success sudo apt-get -y install zip
@@ -12,4 +17,9 @@ run-one-until-success sudo apt-get -y install php
 run-one-until-success sudo apt-get -y install php-xml
 run-one-until-success sudo apt-get -y install php-curl
 run-one-until-success sudo apt-get -y install php-mysql
+
+# Installs and setups 
+#AWS SDK for PHP
+sudo wget -O "$aws_sdk_tmp_location" "$aws_sdk_url"
+sudo unzip "$aws_sdk_tmp_location" -d "$aws_sdk_location"
 
