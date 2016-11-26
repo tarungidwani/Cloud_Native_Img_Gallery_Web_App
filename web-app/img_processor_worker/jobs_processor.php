6ef3d08cea85f6c8df3a08384dee9f43adf4c49f
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-25 23:13:40 -0600

    Added function to submit processed img to finished s3 bucket

--- web-app/img_processor_worker/jobs_processor.php
+++ web-app/img_processor_worker/jobs_processor.php
@@ -1,5 +1,10 @@
 <?php
     require dirname(__DIR__) . '/lib/db_interaction.php';
+    require 'process_img.php';
+    require dirname(__DIR__) . '/lib/config_reader.php';
+    require dirname(__DIR__) . '/lib/s3_interaction.php';
+
+    define("S3CONFIGPATH", dirname(__DIR__) . '/config/s3_connection');
 
     function get_job_to_process($reciept)
     {
@@ -12,4 +17,17 @@
         $job_to_process = execute_query($db_connection_info, $query_to_execute, $err_msg);
 
         return $job_to_process[0];
+    }
+
+    function process_job($job_to_process)
+    {
+        $err_msg = "Failed to read S3 config file";
+        $s3_connection_info = read_info_from_config_file(constant("S3CONFIGPATH"), $err_msg);
+        $raw_img_url = $job_to_process['s3_raw_url'];
+        $finished_bucket_name = $s3_connection_info['finished_bucket_name'];
+        $finished_img_path = process_img($raw_img_url);
+        $region = $s3_connection_info['region'];
+
+        $finished_img_url = submit_file_to_s3($finished_bucket_name, $finished_img_path, $region);
+
     }
