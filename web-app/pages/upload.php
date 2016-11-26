abf2fd78590fb57df649d02308c468085dd0a758
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-26 16:05:20 -0600

    Added logic to check session, print and unset invalid img type err msg, if it is not empty

--- web-app/pages/upload.php
+++ web-app/pages/upload.php
@@ -14,6 +14,15 @@
     <input type="file" accept="image/png,image/jpeg"  name="img_path" style="padding-bottom: 20px;padding-top: 20px;" />
     <br>
     <input type="submit" value="Upload" formaction="uploader.php"/>
+    <br>
+    <?php
+    $invalid_img_type_err_msg = $_SESSION['invalid_img_type_err'];
+    if( !empty($invalid_img_type_err_msg))
+    {
+        echo "<h3>$invalid_img_type_err_msg</h3>";
+        unset($_SESSION['invalid_img_type_err']);
+    }
+    ?>
 </form>
 </body>
 </html>
