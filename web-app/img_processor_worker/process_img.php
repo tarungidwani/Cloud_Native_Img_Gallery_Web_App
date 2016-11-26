4466b64e77fccbb217378484561dcd52a7aadf2f
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-25 20:05:39 -0600

    Added function that creates an img based on its type using php gd ib

--- web-app/img_processor_worker/process_img.php
+++ web-app/img_processor_worker/process_img.php
@@ -17,4 +17,19 @@
         }
         return $raw_img_dest;
     }
+
+    function create_img_based_on_type($raw_img_path)
+    {
+        $img_type = exif_imagetype($raw_img_path);
+
+        if ($img_type == 2)
+            return imagecreatefromjpeg($raw_img_path);
+        elseif($img_type == 3)
+            return imagecreatefrompng($raw_img_path);
+        else
+        {
+            echo "Failed to recognize type/signature of image, please upload another image";
+            exit(1);
+        }
+    }
     
