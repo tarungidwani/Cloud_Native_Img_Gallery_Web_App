f267a2ddc6e6ebc8a35c7f67f61c9ca2d0fab6f7
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-25 20:04:25 -0600

    Updated path to IIT logo img constant and variable names

--- web-app/img_processor_worker/process_img.php
+++ web-app/img_processor_worker/process_img.php
@@ -1,20 +1,20 @@
 <?php
 
-    define("IITLOGOIMG", dirname(__DIR__) . '/IIT-logo.png');
+    define("IITLOGOIMG", __DIR__ . '/IIT-logo.png');
     define("DESTDIR", '/tmp');
 
     function download_img($raw_img_url)
     {
         $img_info = pathinfo($raw_img_url);
         $raw_img_name = $img_info['basename'];
-        $raw_img_dest_dir = constant("DESTDIR") . "/$raw_img_name";
+        $raw_img_dest = constant("DESTDIR") . "/$raw_img_name";
 
-        $is_copy_successful = copy($raw_img_url, $raw_img_dest_dir);
+        $is_copy_successful = copy($raw_img_url, $raw_img_dest);
         if(!$is_copy_successful)
         {
             echo "Failed to download img: $raw_img_name from raw S3 bucket";
             exit(1);
         }
+        return $raw_img_dest;
     }
-
     
