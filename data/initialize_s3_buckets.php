8d1e2ca12c6408ec942ded3a720b7c320addf91d
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-30 14:28:26 -0600

    Defined constants with values to path of images to be pre-seated in the web-app

---
+++ data/initialize_s3_buckets.php
@@ -0,0 +1,11 @@
+<?php
+    /* Paths to imgs that will
+     * pre-seated in this
+     * gallery web app
+     */
+    define("IMG_1_raw"     , dirname(__DIR__) . '/web-app/imgs/eartrumpet.png');
+    define("IMG_1_finished", dirname(__DIR__) . '/web-app/imgs/eartrumpet-bw.png');
+    define("IMG_2_raw"     , dirname(__DIR__) . '/web-app/imgs/Knuth.jpg');
+    define("IMG_2_finished", dirname(__DIR__) . '/web-app/imgs/Knuth-bw.jpg');
+    define("IMG_3_raw"     , dirname(__DIR__) . '/web-app/imgs/mountain.jpg');
+    define("IMG_3_finished", dirname(__DIR__) . '/web-app/imgs/mountain-bw.jpg');
