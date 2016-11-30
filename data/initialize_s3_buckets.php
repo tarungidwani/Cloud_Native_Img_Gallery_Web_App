e3acec57eec2a6c698fa586189710a255f138b98
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-30 14:29:56 -0600

    Created function that prepares a sql stmt to insert a record into the img jobs table

--- data/initialize_s3_buckets.php
+++ data/initialize_s3_buckets.php
@@ -9,3 +9,16 @@
     define("IMG_2_finished", dirname(__DIR__) . '/web-app/imgs/Knuth-bw.jpg');
     define("IMG_3_raw"     , dirname(__DIR__) . '/web-app/imgs/mountain.jpg');
     define("IMG_3_finished", dirname(__DIR__) . '/web-app/imgs/mountain-bw.jpg');
+
+    function setup_prepared_statement($mysql_connection, $jobs_table_name)
+    {
+        $insert_raw_img_record_stmt = $mysql_connection->prepare("INSERT INTO $jobs_table_name VALUES (?,?,?,?,?,?,?)");
+
+        if(!$insert_raw_img_record_stmt)
+        {
+            echo "Failed to insert raw img job record (*Prepare failed: " . $mysql_connection->error . "*)";
+            $mysql_connection->close();
+            exit(1);
+        }
+        return $insert_raw_img_record_stmt;
+    }
