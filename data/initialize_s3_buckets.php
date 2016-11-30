6114b20a2ddc405aa2f7c30943abb145a57a1fea
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-11-30 14:31:25 -0600

    Created function to bind img raw and finished urls to the insert img job record prepared stmt

--- data/initialize_s3_buckets.php
+++ data/initialize_s3_buckets.php
@@ -22,3 +22,21 @@
         }
         return $insert_raw_img_record_stmt;
     }
+
+    function bind_multiple_params_to_prepared_stmt($stmt, $s3_raw_url, $s3_finished_url)
+    {
+        $id = NULL;
+        $user_login_id = 2;
+        $phone_number = '';
+        $status = '1';
+        $reciept = md5($s3_raw_url);
+
+        $stmt->bind_param("sisssss", $id ,$user_login_id, $phone_number, $s3_raw_url, $s3_finished_url, $status, $reciept);
+
+        if(!stmt)
+        {
+            echo "Failed to insert raw img job record (*Bind failed: " . $stmt->error . "*)";
+            exit(1);
+        }
+        return $stmt;
+    }
