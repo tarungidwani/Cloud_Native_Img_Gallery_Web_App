5be55ef070356133ed276d51c62a574f9162a910
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-12-01 13:00:57 -0600

    Added code to check the status (enabled or disabled) of upload feature and based upon that display the appropriate buttons or message for user

--- web-app/pages/upload.php
+++ web-app/pages/upload.php
@@ -1,6 +1,7 @@
 <?php
     require 'init.php';
     require 'menu.php';
+    require dirname(__DIR__) . '/lib/feature_status.php';
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +13,19 @@
 <body>
 <form method="post" enctype="multipart/form-data">
     <?php
+        $feature_name = 'upload';
+        $is_upload_feature_enabled = is_feature_enabled($feature_name);
+
+        if($is_upload_feature_enabled)
+        {
+            echo "<input type=\"file\" accept=\"image/png,image/jpeg\"  name=\"img_path\" style=\"padding-bottom: 20px;padding-top: 20px;\" />";
+            echo "<br>";
+            echo "<input type=\"submit\" value=\"Upload\" formaction=\"uploader.php\"/>";
+            echo "<br>";
+        }
+        else
+            echo "<h3>Upload feature is currently unavailable due to maintenance, please check back again later</h3>";
+
         $invalid_img_type_err_msg = $_SESSION['invalid_img_type_err'];
         if( !empty($invalid_img_type_err_msg))
         {
