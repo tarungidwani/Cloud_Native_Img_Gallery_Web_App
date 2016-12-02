5240790e6c2d900ccacc855f340548bc4098d23e
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-12-01 19:14:37 -0600

    Created basic html document structure for admin page

--- web-app/pages/admin.php
+++ web-app/pages/admin.php
@@ -6,3 +6,13 @@
         header('Location: welcome.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+</head>
+<body>
+</body>
+</html>
+
