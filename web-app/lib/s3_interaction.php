84be16d6f76ce1f90574700ebc153c17ea2f499d
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-12-02 14:52:04 -0600

    Created function to upload file to s3 bucket with acl: private

--- web-app/lib/s3_interaction.php
+++ web-app/lib/s3_interaction.php
@@ -28,3 +28,30 @@
             exit(1);
         }
     }
+
+    /* Uploads a file to the specified
+     * AWS S3 bucket ACL: private
+     */
+    function submit_file_to_s3_private($bucket_name, $file_location, $region)
+    {
+        $s3_client = new \Aws\S3\S3Client([
+            'version' => 'latest',
+            'region'  => "$region"
+        ]);
+
+        try
+        {
+             $s3_client->putObject([
+                'Bucket'     => $bucket_name,
+                'Key'        => basename($file_location),
+                'SourceFile' => $file_location,
+                'ACL'        => 'private'
+            ]);
+        }
+        catch(\Aws\S3\Exception\S3Exception $s3_exception)
+        {
+            echo "Failed to upload db back up to $bucket_name in S3\n";
+            exit(1);
+        }
+    }
+
