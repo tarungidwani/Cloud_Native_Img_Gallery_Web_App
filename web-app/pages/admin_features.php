f8f9c529e80949035de08ba042d29aad77201e81
Tarun Gidwani <tgidwani@hawk.iit.edu>
2016-12-02 15:01:28 -0600

    Created function that backs up the db used by the web-app using the mysqldump utility

--- web-app/pages/admin_features.php
+++ web-app/pages/admin_features.php
@@ -1,6 +1,7 @@
 <?php
     require 'init.php';
     require dirname(__DIR__) . '/lib/feature_status.php';
+    include_once dirname(__DIR__) . '/lib/db_interaction.php';
 
     function update_upload_feature_status()
     {
@@ -18,6 +19,19 @@
         $_SESSION['upload_feature_status_msg'] = "Successfully $status $feature_name feature in web-app";
         header('Location: admin.php');
         exit(0);
+    }
+
+    function create_db_back_up()
+    {
+        $db_connection_info = setup_db_info();
+        $db_endpoint = $db_connection_info['db_endpoint'];
+        $db_username = $db_connection_info['db_username'];
+        $db_password = $db_connection_info['db_password'];
+        $db_name = $db_connection_info['db_name'];
+        $back_up_file = $db_name . "_back_up.sql";
+
+        $backup_command = "mysqldump --opt -h $db_endpoint -u $db_username -p$db_password $db_name > $back_up_file 2> /dev/null";
+        system($backup_command);
     }
 
     $value_submitted = $_POST['submit'];
